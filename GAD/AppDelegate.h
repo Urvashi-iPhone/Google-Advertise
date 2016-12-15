29afeb81752410d558226f356aa4038f77c2f8fc
Tecksky Techonologies <tecksky@teckskys-mac-mini.local>
2016-12-15 17:05:02 +0530

    Initial Commit

---
+++ GAD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GAD
+//
+//  Created by Tecksky Techonologies on 12/15/16.
+//  Copyright © 2016 Softranz Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
